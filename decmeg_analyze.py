e25ee7a73ad60cf141dd5efd0f449308623d2021
Dan <ecodan@hotmail.com>
2014-05-09 21:01:43 -0600

    initial checkin

---
+++ decmeg_analyze.py
@@ -0,0 +1,141 @@
+__author__ = 'dan'
+
+import pandas as pd
+import numpy as np
+import os
+from sklearn.decomposition import PCA
+from datetime import datetime
+from scipy.io import loadmat
+import sklearn as sl
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import roc_auc_score
+from sklearn.decomposition import ProbabilisticPCA
+import sklearn.preprocessing as pre
+import re
+
+
+in_dir = '/Users/dan/dev/datasci/kaggle/decmeg2014/'
+
+# file_suffix = '.mat.pca.csv'
+file_suffix = '.mat2.csv'
+
+def load_data(dir):
+    print('loading data from ' + dir)
+    files = os.listdir(dir)
+    files.sort()
+    # concat all files
+    data = []
+    for file in files:
+        if file.endswith(file_suffix) == False:
+            continue
+        print('loading ' + file)
+        if len(data) == 0:
+            data = np.loadtxt(dir + '/' + file, delimiter=',')
+        else:
+            data = np.vstack([data, np.loadtxt(dir + '/' + file, delimiter=',')])
+    return data
+
+
+def evaluate_models(train_dir):
+    print('reading files')
+    data = load_data(train_dir)
+
+    print('raw data ' + str(data.shape))
+    X = data[:,0:-1]
+    print('X ' + str(X.shape))
+    y = data[:,-1::]
+    print('y ' + str(y.shape))
+
+    for C in [.01,.1,1,10]:
+        for p in ['l1','l2']:
+            print('C=' + str(C) + ' p=' + p)
+            clf = LogisticRegression(C=C,penalty=p)
+            print('cross validating')
+            scores = sl.cross_validation.cross_val_score(clf, X, y.ravel(), scoring='accuracy')
+            mscores = np.mean(scores)
+            print('ref xval scores=' + str(scores) + ' | mean=' + str(mscores))
+
+    # random forest - not working so well on this
+    # for e in [5,10,25]:
+    #     for c in ['gini','entropy']:
+    #         for f in [.5,.75,1.0]:
+    #             print (': evaluating e=' + str(e) + ' c=' + c + ' f=' + str(f))
+    #             clf = RandomForestClassifier(n_estimators=e,criterion=c, max_features=f)
+    #             scores = sl.cross_validation.cross_val_score(clf, X, y.ravel(), scoring='roc_auc')
+    #             mscores = np.mean(scores)
+    #             print(': RF xval scores=' + str(scores) + ' | mean=' + str(mscores))
+    #             # if mscores > best_auc:
+    #             #     best_auc = mscores
+
+    # gradient boost
+    # for e in [50,100,200]:
+    #         for f in [.5,.75,1.0]:
+    #             print (': evaluating e=' + str(e) + ' f=' + str(f))
+    #             clf = GradientBoostingClassifier(n_estimators=e, max_features=f, verbose=True)
+    #             scores = sl.cross_validation.cross_val_score(clf, X, y.ravel(), scoring='roc_auc')
+    #             mscores = np.mean(scores)
+    #             print(': GB xval scores=' + str(scores) + ' | mean=' + str(mscores))
+    #             # if mscores > best_auc:
+    #             #     best_auc = mscores
+
+    # create training and test data (.75/.25 split)
+    X_train, X_test, y_train, y_test = sl.cross_validation.train_test_split(X, y)
+    clf.fit(X_train, y_train.ravel())
+    z = clf.predict(X_test)
+    print ('TRAIN conf matrix:\n' + str(sl.metrics.confusion_matrix(y_test, z)))
+    print ('TRAIN class report:\n' + str(sl.metrics.classification_report(y_test, z)))
+
+
+def train(train_dir):
+    print ('training...')
+    data = load_data(train_dir)
+
+    X = data[:,0:-1]
+    print('X ' + str(X.shape))
+    y = data[:,-1::]
+    print('y ' + str(y.shape))
+
+    clf = LogisticRegression(C=1,penalty='l2')
+    clf.fit(X, y.ravel())
+
+    X_train, X_test, y_train, y_test = sl.cross_validation.train_test_split(X, y)
+    z = clf.predict(X_test)
+    print ('TRAIN conf matrix:\n' + str(sl.metrics.confusion_matrix(y_test, z)))
+    print ('TRAIN class report:\n' + str(sl.metrics.classification_report(y_test, z)))
+
+    return clf
+
+def predict(test_dir, clf):
+    print ('predicting...')
+
+    files = os.listdir(test_dir)
+    files.sort()
+    output = np.zeros((4058,2), dtype=np.int)
+    idx = 0
+    for file in files:
+        if file.endswith(file_suffix) == False:
+            continue
+        print('loading ' + file)
+        data = np.loadtxt(test_dir + '/' + file, delimiter=',')
+        z = clf.predict(data)
+        m = re.match("test_subject(.*)\.mat", file)
+        uid = m.group(1)
+        userid = int(uid)
+        for i in range(0, len(z)):
+            output[idx][0] = userid * 1000 + i
+            output[idx][1] = z[i]
+            idx += 1
+    df = pd.DataFrame(output)
+    df.columns = ['Id', 'Prediction']
+    df.to_csv(test_dir + '/predictions.csv', index=False, header=True)
+
+evaluate_models(in_dir + 'train/out/')
+
+# clf = train(in_dir + '/train/out')
+
+# predict(in_dir + '/test/out', clf)
---
+++ decmeg_munge_2.py
@@ -0,0 +1,93 @@
+__author__ = 'dan'
+
+'''
+Step 1 in pipeline
+
+This approach averages the brain data for each time slice accross all sensors
+
+Input: decmeg matlab files
+
+Output:
+1) diff'd time series (delta between each two time frames)
+2) descretized time series (deltas converted to binary with threshold N)
+
+'''
+
+import pandas as pd
+import numpy as np
+import os
+from sklearn.decomposition import PCA
+from datetime import datetime
+from scipy.io import loadmat
+
+in_dir = '/Users/dan/dev/datasci/kaggle/decmeg2014/'
+num_components = 5000
+
+def munge(in_dir):
+
+    pca = PCA(n_components=num_components)
+
+    # loop through all matlab files in in_dir
+    i = 0
+    for dir in ['/train','/test']:
+        out_dir = in_dir + dir + '/out/'
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        files = os.listdir(in_dir + dir)
+        files.sort()
+        for file in files:
+            if file.endswith('.mat'):
+                i += 1
+
+                # read matlab file
+                in_file = in_dir + dir + '/' + file
+                print('reading file ' + in_file + ' at ' + str(datetime.now()))
+
+                fmat = loadmat(in_file)
+
+                sfreq = fmat['sfreq']
+                tmin = fmat['tmin']
+                tmax = fmat['tmax']
+                print('f=' + str(sfreq) + ' | min=' + str(tmin) + ' | max=' + str(tmax))
+
+                X = np.array(fmat['X'])
+                Xs = np.array(X[:,:,75::])
+                Xf = np.zeros((np.shape(Xs)[0], np.shape(Xs)[1]))
+                for idx in range(0,np.shape(Xs)[0]):
+                    Xf[idx] = np.mean(Xs[idx], axis=1)
+                Xf -= Xf.mean(0)
+                Xf = np.nan_to_num(Xf / Xf.std(0))
+                print('X=' + str(np.shape(X)) + ' | Xs=' + str(np.shape(Xs)) + ' | Xf=' + str(np.shape(Xf)))
+
+                y = []
+                data = []
+
+                if dir == '/train':
+                    y = np.array(fmat['y'])
+                    print('y=' + str(np.shape(y)))
+                    data = np.hstack([Xf, y])
+                else:
+                    data = Xf
+
+                print('data=' + str(np.shape(data)))
+                np.savetxt(out_dir + file + '2.csv', data, delimiter=',',fmt='%1.3f')
+
+                if i == 1:
+                    # train PCA
+                    print('training PCA')
+                    pca.fit(Xf)
+
+                print('transforming to PCA')
+                Xp = pca.transform(Xf)
+                Xp -= Xp.mean(0)
+                Xp = np.nan_to_num(Xp / Xp.std(0))
+                if dir == '/train':
+                    data = np.hstack([Xp, y])
+                else:
+                    data = Xp
+                print('data PCA=' + str(np.shape(data)))
+                np.savetxt(out_dir + file + '2.pca.csv', data, delimiter=',',fmt='%1.3f')
+
+
+munge(in_dir)
